f87db7382e96f08f71c10537c71ea2d0dc1d83a2
Fumitoshi Ogata <ogata.fumitoshi@o-09353-mac.local>
2017-12-13 16:02:57 +0900

    fixed bug

--- routes/routes.js
+++ routes/routes.js
@@ -404,7 +404,7 @@ app.get('/api/user/buy',function(req,res){
                 });
             }
             User = user[0];
-            buycoin(User,amount,res);
+            buycoin(User,coinAmount,res);
         }
     );
 });
